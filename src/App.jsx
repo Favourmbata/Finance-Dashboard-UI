0e7d5ae0a3f219c93ce24806f911f1d4814e42e6
favour.mbata@raoatech.com <favour.mbata@raoatech.com>
2026-04-03 07:39:36 +0100

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function Layout() {
   return (
     <div className={`flex h-screen overflow-hidden ${darkMode ? 'bg-gray-950' : 'bg-gray-50'}`}>
       <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+        {/* comment */}
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Header onMenuClick={() => setSidebarOpen(true)} />
         <main className="flex-1 overflow-y-auto p-4 sm:p-6">
